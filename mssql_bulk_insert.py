2ef9e5d8a850baa2fc52cba8fb8ff270e37e500a
Aaron Retsky <anretsky@umich.edu>
2019-06-23 14:01:06 +0300

    Add imports to mssql_bulk_insert.py and change method to use temp file instead of hardcoded file location
    Also commented on the case of running MSSQL Server on macOS in Docker container

--- mssql_bulk_insert.py
+++ mssql_bulk_insert.py
@@ -1,16 +1,27 @@
+import sys
+import os
+import csv
+from tempfile import NamedTemporaryFile
+
 def mssql_bulk_insert(table, conn, keys, data_iter):
     # gets a DBAPI connection that can provide a cursor
-    with open('tmp_data.csv', 'w') as csvfile:
+    with NamedTemporaryFile() as csvfile:
         writer = csv.writer(csvfile)
         writer.writerows(data_iter)
-    dbapi_conn = conn.connection
-    with dbapi_conn.cursor() as cur:
-        table_name = '[dbo].[{}]'.format(table.name)
-        command = '''BULK INSERT {}
-            FROM {}
-            WITH (FORMAT = 'CSV',
-            FIELDTERMINATOR = ','
-            ) '''.format(table_name, r"'/opt/mssql/data.csv'")
-        # shared data file must be copied to non-shared file for bulk insert to work
-        os.system("docker exec sql1 cp opt/mssql/tmp_data.csv opt/mssql/data.csv")
-        cur.execute(command)
+        dbapi_conn = conn.connection
+        tempfile = csvfile.name
+        with dbapi_conn.cursor() as cur:
+            table_name = '[dbo].[{}]'.format(table.name)
+            command = '''BULK INSERT {}
+                FROM {}
+                WITH (FORMAT = 'CSV',
+                FIELDTERMINATOR = ','
+                ) '''.format(table_name, tempfile)
+            
+            # For MSSQL Server running on macOS in docker container:
+            # csvfile must be shared with docker container and shared
+            # data file must be copied to non-shared file for bulk
+            # insert to work. Use line below to copy shared file:
+            # os.system("docker exec sql1 cp opt/mssql/{}  opt/mssql/data.csv".format(tempfile))
+            
+            cur.execute(command)
